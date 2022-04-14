39ca195e240727c362957e8049fc4d7942af7bdb
DimaLuchyk <dimaluchik7@gmail.com>
2022-04-14 14:50:53 +0300

    Added constants for NetworkManager

---
+++ ISensor/GlobalThings/GlobalThings.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_THINGS
+#define GLOBAL_THINGS
+
+#include "SettingManager/SettingManager.h"
+
+//ini file
+static const char* g_cszSettingsFile = "ISensorSettings.ini";
+
+static const char* g_cszNetworkManagerSection = "NetworkManager";
+static const char* g_cszUrlKey = "url";
+static const char* g_cszDataFileKey = "dataFile";
+
+#endif //GLOBAL_THINGS
